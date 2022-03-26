8679ce1401b4f8927d355ce283ff348b4dc4cee3
marcon29 <online@markstabler.com>
2022-03-26 08:44:18 -0600

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(timeString) {
+  const hour = parseInt(timeString.split(":")[0]);
+  let greeting = "Good Morning"
+
+  if (hour >= 17) {
+      greeting = "Good Evening"
+    } else if (hour >= 12) {
+      greeting = "Good Afternoon"
+  }
+
+  return greeting
+}
+
+function displayMessage(greetingMsg) {
+  const greetingNode = document.getElementById("greeting");
+  greetingNode.textContent = greetingMsg;
+}
+
